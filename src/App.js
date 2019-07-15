cc35262f1925d09633d4de1dc9152c375455bdd7
Student <jsitbon@qcc.cuny.edu>
2019-07-15 17:44:40 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodoList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      currentToDo: "",
+    }
+  }
+
+
+
+  // <div className = "navbar">
+  // <a  href = "">Home</a>
+  // <a href = "">Contact</a>
+  // <a href = "">Paperwork</a>
+  // <a href = ""> About</a>
+  // </div>
+
+
+
+  // <ul>
+  // <li>firstname</li>
+  // <li>lastname</li>
+  // <li>email</li>
+  // <li>phonenumber</li>
+  // </ul>
+
+
+  handleChange = event => {
+    event.preventDefault();
+    this.setState({
+      currentToDo: event.target.value
+    })
+    //set state for the current todo
+    console.log(event.target.value);
+  }
+
+
+
+  
+  addItem = event => {
+   if(currentToDo != ""){
+console.log("no answer")
+
+    
+   }
+
+  
+   }
+   this.setState({
+
+    todos: event.target.value
+   })
+
+   if(currentToDo = ""){
+    console.log(todos.push(currentToDo)
+
+    //set state for the todo array and build that array with the currentTodo
+
+    event.preventDefault();
+    
+  }
+
+
+  render() {
+    console.log(this.state.todos);
+    return (
+      <div className='container'>
+        <form onSubmit={this.addItem}>
+          <label htmlFor="taskName"> Test Name: </label>
+          <input onChange={this.handleChange} name="taskname" type="text" placeholder="Add todo here" />
+          <button type="submit"> Add Task</button>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default TodoList;
