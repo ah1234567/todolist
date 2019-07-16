b67c31381f2c5a88942739ac5f901eeb3fc12541
Student <jsitbon@qcc.cuny.edu>
2019-07-16 17:12:11 -0400

    update2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class TodoList extends Component {
   constructor() {
     super();
     this.state = {
-      todos: [],
+      todos: [], //array of strings
       currentToDo: "",
     }
   }
@@ -19,63 +19,145 @@ class TodoList extends Component {
   // </div>
 
 
-
+  // <div className = "list">
   // <ul>
   // <li>firstname</li>
   // <li>lastname</li>
   // <li>email</li>
   // <li>phonenumber</li>
   // </ul>
+  // </div>
+
+
 
 
   handleChange = event => {
     event.preventDefault();
+
     this.setState({
-      currentToDo: event.target.value
+      currentToDo: event.target.value,
     })
-    //set state for the current todo
-    console.log(event.target.value);
+
   }
 
 
 
-  
+
   addItem = event => {
-   if(currentToDo != ""){
-console.log("no answer")
+    event.preventDefault()
+    //    if(currentToDo != ""){
+    // console.log("no answer")
+    // }
+    this.setState({ todos: [...this.state.todos, this.state.currentToDo], currentToDo: "" })
 
-    
-   }
 
-  
-   }
-   this.setState({
-
-    todos: event.target.value
-   })
-
-   if(currentToDo = ""){
-    console.log(todos.push(currentToDo)
-
-    //set state for the todo array and build that array with the currentTodo
-
-    event.preventDefault();
-    
   }
+
+
+
+  deleteItem = (index) => {
+    console.log(index)
+    this.setState({
+      todos: this.state.todos.filter((el, idx) => {
+
+        return el != index;
+      })
+    })
+  }
+  //filter for elements whose index is NOT the index we are deleting
+
+
+  //find out what the current todo is
+  // add that toDo to the Array via this.setState()
+
+
+  // todos: event.target.value,
+  // addItem: event.target.value}
+
+
+  //whatever the todo is// ...todos, currentTodo }) sort of like .push() //this.state.todos.push() is a no no. We dont directly change the state, we always call this.setState()
+
+  //set state for the todo array and build that array with the currentTodo
+
+
 
 
   render() {
-    console.log(this.state.todos);
+    console.log(this.state);
     return (
-      <div className='container'>
-        <form onSubmit={this.addItem}>
-          <label htmlFor="taskName"> Test Name: </label>
-          <input onChange={this.handleChange} name="taskname" type="text" placeholder="Add todo here" />
-          <button type="submit"> Add Task</button>
-        </form>
+      <div>
+        <div className='container'>
+          <form onSubmit={this.addItem}>
+            <label htmlFor="taskName"> Test Name: </label>
+            <input onChange={this.handleChange} name="taskname" type="text" placeholder="Add todo here" />
+            <input onChange={this.addItem} name="additems" type="button" />
+            <button type="submit"> Add Task</button>
+
+            {/* <input onChange = {this.deleteItem} name = "delete" type = "text" placeholder = "delete task"/> */}
+            {/* <button onClick = {this.addItem}>
+          {this.state.isTodoliston? "True" : "false"}</button> */}
+          </form>
+        </div>
+        <ul className="list1">
+          {this.state.todos.map((el, index) =>
+            <li key={index}>
+              {el}
+              <button onClick={() => this.deleteItem(index)}>delete</button>
+            </li>)}
+        </ul>
       </div>
+
     )
   }
 }
+
+
+
+
+
+
+// class TasksList extends Component {
+//   constructor() {
+//     super();
+//     this.state.props = {
+//   listname: "list1"
+
+
+
+//     render() {
+//       console.log(this.state);
+//       return (
+//         <div>
+//         <div className='container'>
+//           <form onSubmit={this.addItem}>
+//             <label htmlFor="taskName"> Test Name: </label>
+//             <input onChange={this.handleChange} name="taskname" type="text" placeholder="Add todo here" />
+//             <input onChange = {this.addItem} name = "additems" type = "button" />
+//            <button type="submit"> Add Task</button>
+
+//            {/* <input onChange = {this.deleteItem} name = "delete" type = "text" placeholder = "delete task"/> */}
+//             {/* <button onClick = {this.addItem}>
+//             {this.state.isTodoliston? "True" : "false"}</button> */}
+//           </form>
+//           </div>
+//           <ul className = "list1">
+//             {this.state.todos.map(el =>
+//               <li key={el}>
+//                 {el}
+//                 <button onClick={this.deleteItem}>delete</button>
+//               </li>)}
+//           </ul>
+//         </div>
+//       )
+//     }
+
+
+//   }
+//     }
+//   }
+
+
+
+
 
 export default TodoList;
