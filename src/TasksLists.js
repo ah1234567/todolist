ff96b87847d8d0bdf58a712d605749f76119b2f3
ah1234567 <abdullahhasanbusiness@gmail.com>
2019-07-19 13:02:50 -0400

    5th update

--- src/TasksLists.js
+++ src/TasksLists.js
@@ -1,34 +1,3 @@
-// import React from 'react';
-// // import { StyleSheet } from 'react-native';
-
-
-
-// const TaskLists = (props) => {
-//   return (
-//     <div>
-//     <ul>
-//       {props.todos.map((el, index) =>
-//         <li key={index}>
-//           {el}
-//           <button onClick={() => props.deleteItem(index)}>delete</button>
-//         </li>)}
-//         </ul>
-//     </div>
-//   )
-// }
-
-
-// export default TaskLists
-
-
-
-
-
-
-
-
-
-
 import React from 'react';
 // import { StyleSheet } from 'react-native';
 const TaskLists = (props) => {
